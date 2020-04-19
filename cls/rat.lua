c61b7dc6a5549c9d84ef796189acf957cc6e6019
IMP1 <huw_taylor@hotmail.co.uk>
2020-04-19 17:45:23 +0100

    Add state to rat

--- cls/rat.lua
+++ cls/rat.lua
@@ -7,16 +7,29 @@ local SPRITES = {
     idle = love.graphics.newImage("gfx/rat_1.png"),
 }
 
+local GRAZE_TIME_PERIOD = {1, 4}
+
 function rat.new(x, y)
     local self = {}
     setmetatable(self, rat)
     self.position = vec2.new(x, y)
     self.sprite = SPRITES.idle
     self.size = {}
+    self.grazing = true
+    self.fleeing = true
+    self.moving  = true
+    self.timer   = GRAZE_TIME_PERIOD[1] + (GRAZE_TIME_PERIOD[2] - GRAZE_TIME_PERIOD[1]) * math.random()
     return self
 end
 
-function rat:update(dt)
+function rat:update(dt, player)
+    if self.grazing then
+        self.timer = self.timer - dt
+        if self.timer <= 0 then
+            self.timer = 0
+            -- TODO: move somewhere else.
+        end
+    end
 end
 
 function rat:draw()
