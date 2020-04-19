9561c477021dc88c81bfd0aeee4c942bcd84be94
IMP1 <huw_taylor@hotmail.co.uk>
2020-04-19 15:36:20 +0100

    Use name parameter

--- cls/hiding_spot.lua
+++ cls/hiding_spot.lua
@@ -3,9 +3,10 @@ local vec2 = require 'lib.vector2'
 local hiding_spot = {}
 hiding_spot.__index = hiding_spot
 
-function hiding_spot.new(x, y, w, h)
+function hiding_spot.new(name, x, y, w, h)
     local self = {}
     setmetatable(self, hiding_spot)
+    self.name = name
     self.position = vec2.new(x, y)
     self.size = vec2.new(w, h)
     return self
