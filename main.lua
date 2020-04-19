2dc37c8c05de438f8fc872cdad1ab4b1271a106b
IMP1 <huw_taylor@hotmail.co.uk>
2020-04-19 14:20:09 +0100

    Add pixel-perfect image filter

--- main.lua
+++ main.lua
@@ -1,14 +1,22 @@
 local scene_manager = require 'lib.scene_manager'
 
-local ForestScene = require 'scn.forest'
-
 function T(str)
     return str
 end
 
+local PIXEL_SCALE = 1
+local WIDTH, HEIGHT = love.graphics.getWidth(), love.graphics.getHeight()
+local canvas
+
 function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    local ForestScene = require 'scn.forest'
     scene_manager.hook()
     scene_manager.setScene(ForestScene.new())
+end
+
+function love.keypressed(key)
+    if key == "escape" then love.event.quit() end
 end
 
 function love.update(dt)
