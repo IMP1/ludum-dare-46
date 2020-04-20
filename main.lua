817be5f7580749981ffa0b3bc26f8c5800ca5393
IMP1 <huw_taylor@hotmail.co.uk>
2020-04-20 18:27:20 +0100

    Add tutorial table

--- main.lua
+++ main.lua
@@ -10,6 +10,17 @@ local PIXEL_SCALE = 1
 local WIDTH, HEIGHT = love.graphics.getWidth(), love.graphics.getHeight()
 local canvas
 
+TUTORIAL = {
+    timers = {
+        movement = nil,
+        swooping = nil,
+    },
+    movement_displayed = false,
+    swooping_displayed = false,
+}
+
+-- TODO: Add prompts for gameplay
+
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
     local pixel_font = love.graphics.newImageFont("gfx/font.png", "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz1234567890!?.,:;'()-", 1) 
@@ -20,11 +31,18 @@ function love.load()
 end
 
 function love.keypressed(key)
-    if key == "escape" then love.event.quit() end
+    if DEBUG then
+        if key == "escape" then love.event.quit() end
+    end
     if key == "`" then DEBUG = not DEBUG end
 end
 
 function love.update(dt)
+    for key, timer in pairs(TUTORIAL.timers) do
+        if timer then
+            TUTORIAL.timers[key] = timer + dt
+        end
+    end
     scene_manager.update(dt)
 end
 
